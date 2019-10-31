e48aedf0d59bb7bbc11851404fdf4d7940843c52
SkecisAI <skecis@qq.com>
2019-10-31 13:14:28 +0800

    Site updated: 2019-10-31 13:14:24

--- js/src/love.js
+++ js/src/love.js
@@ -1,73 +1,42 @@
-/* 爱心特效 */
+(function() {
 
-!function (e, t, a) {
-  function r() {
-    for (var e = 0; e < s.length; e++) s[e].alpha <= 0 ? (t.body.removeChild(s[e].el), s.splice(e, 1)) : (s[e].y--, s[e].scale += .004, s[e].alpha -= .013, s[e].el.style.cssText = "left:" + s[e].x + "px;top:" + s[e].y + "px;opacity:" + s[e].alpha + ";transform:scale(" + s[e].scale + "," + s[e].scale + ") rotate(45deg);background:" + s[e].color + ";z-index:99999");
-    requestAnimationFrame(r)
-  }
+    var T_color = "";//字体颜色,你不设置就是随机颜色,
 
-  function n() {
-    var t = "function" == typeof e.onclick && e.onclick;
-    e.onclick = function (e) {
-      t && t(), o(e)
-    }
-  }
+    var T_size = [10,20];//文字大小区间,不建议太大
 
-  function o(e) {
-    var a = t.createElement("div");
-    a.className = "heart", s.push({
-      el: a,
-      x: e.clientX - 5,
-      y: e.clientY - 5,
-      scale: 1,
-      alpha: 1,
-      color: c()
-    }), t.body.appendChild(a)
-  }
+    var T_font_weight = "bold";//字体粗斜度-->normal,bold,900
 
-  function i(e) {
-    var a = t.createElement("style");
-    a.type = "text/css";
-    try {
-      a.appendChild(t.createTextNode(e))
-    } catch (t) {
-      a.styleSheet.cssText = e
-    }
-    t.getElementsByTagName("head")[0].appendChild(a)
-  }
+    var AnimationTime = 1500;//文字消失总毫秒
 
-  function c() {
-    return "rgb(" + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + ")"
-  }
-  var s = [];
-  e.requestAnimationFrame = e.requestAnimationFrame || e.webkitRequestAnimationFrame || e.mozRequestAnimationFrame || e.oRequestAnimationFrame || e.msRequestAnimationFrame || function (e) {
-    setTimeout(e, 1e3 / 60)
-  }, i(".heart{width: 10px;height: 10px;position: fixed;background: #f00;transform: rotate(45deg);-webkit-transform: rotate(45deg);-moz-transform: rotate(45deg);}.heart:after,.heart:before{content: '';width: inherit;height: inherit;background: inherit;border-radius: 50%;-webkit-border-radius: 50%;-moz-border-radius: 50%;position: fixed;}.heart:after{top: -5px;}.heart:before{left: -5px;}"), n(), r()
-}(window, document);
+    var Move_up_Distance = 388;//文字移动距离,正数代表上移，反之下移
 
-/* 社会主体核心价值观效果 */
-var a_idx = 0;
-jQuery(document).ready(function ($) {
-  $("body").click(function (e) {
-    var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
-    var $i = $("<span/>").text(a[a_idx]);
-    a_idx = (a_idx + 1) % a.length;
-    var x = e.pageX,
-      y = e.pageY;
-    $i.css({
-      "z-index": 100000000,
-      "top": y - 20,
-      "left": x,
-      "position": "absolute",
-      "font-weight": "bold",
-      "color": "#ff6651"
+    var a_index = 0;
+    $("html").click(function(e){
+        var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
+        var $i = $("<span/>").text(a[a_index]);
+        a_index = (a_index + 1) % a.length;
+        var x = e.pageX,y = e.pageY;
+        var x_color =  "#" + ('00000' + (Math.random() * 0x1000000 << 0).toString(16)).substr(-6);//-->随机颜色
+        //console.log(x_color);
+        if(T_color.length>=4){
+            x_color = T_color;
+        }
+
+        var x_size = Math.random()*(T_size[1]-T_size[0]) + T_size[0];
+        x_size +=  "px";
+
+        $i.css({
+            "z-index": 99999,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "font-size":x_size,
+            "color": x_color
+        });
+        $("html").append($i);
+        $i.animate({"top": y-Move_up_Distance,"opacity": 0},AnimationTime,function() {
+            $i.remove();
+        });
     });
-    $("body").append($i);
-    $i.animate({
-      "top": y - 180,
-      "opacity": 0
-    }, 1500, function () {
-      $i.remove();
-    });
-  });
-});
+})();
